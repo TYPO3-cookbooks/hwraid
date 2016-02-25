eceffaa52fe255ea444333ceb5f9feb45ee92537
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-25 08:42:09 +0100

    Bump version to 1.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures hwraid"
-version          "1.1.1"
+version          "1.1.2"
 
 depends "apt"
